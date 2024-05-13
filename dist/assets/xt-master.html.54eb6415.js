ea7468620d631f55bdc61f8d3b19c7616ca67793
zenasas <164794428@qq.com>
2024-05-13 13:33:20 +0800

    子模块提交修改文件

---
+++ dist/assets/xt-master.html.54eb6415.js
@@ -0,0 +1,2 @@
+const e = JSON.parse('{"key":"v-1bf725db","path":"/lora/xt-master.html","title":"LoRA \u5c0f\u5510\u7248\u81ea\u884c\u8f66\uff08\u0053\u0044\uff09","lang":"en-US","frontmatter":{"example":true,"trainType":"xt-lora-master"},"excerpt":"","headers":[],"filePathRelative":"lora/xt-master.md"}');
+export {e as data};
---
+++ dist/assets/xt-master_sdxl.html.54eb6415.js
@@ -0,0 +1,2 @@
+const e = JSON.parse('{"key":"v-1bf725de","path":"/lora/xt-master_sdxl.html","title":"LoRA \u5c0f\u5510\u7248\u81ea\u884c\u8f66\uff08\u0053\u0044\u0058\u004c\uff09","lang":"en-US","frontmatter":{"example":true,"trainType":"xt-lora-master_sdxl"},"excerpt":"","headers":[],"filePathRelative":"lora/xt-master_sdxl.md"}');
+export {e as data};

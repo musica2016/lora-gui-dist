6e4611a4d38e3c5c009d65ab0282d016cc058321
zenasas <164794428@qq.com>
2024-05-13 13:24:46 +0800

    微调UI

---
+++ dist/assets/xt-master.html.176a0423.js
@@ -0,0 +1,15 @@
+import {_ as a, o as t, c as r, a as e, b as o} from "./app.b7461f39.js";
+
+const s = {}, c = e("h1", {id: "lora-\u5c0f\u5510\u7248\u81ea\u884c\u8f66\uff08\u0053\u0044\uff09", tabindex: "-1"}, [e("a", {
+        class: "header-anchor",
+        href: "#lora-\u5c0f\u5510\u7248\u81ea\u884c\u8f66\uff08\u0053\u0044\uff09",
+        "aria-hidden": "true"
+    }, "#"), o(" LoRA \u5c0f\u5510\u7248\u81ea\u884c\u8f66\uff08\u0053\u0044\uff09")], -1),
+    _ = e("p", null, "\u4f60\u6240\u70ed\u7231\u7684\u0020\u5c31\u662f\u4f60\u7684\u81ea\u884c\u8f66", -1), n = [c, _];
+
+function d(l, i) {
+    return t(), r("div", null, n)
+}
+
+var f = a(s, [["render", d], ["__file", "xt-master.html.vue"]]);
+export {f as default};
